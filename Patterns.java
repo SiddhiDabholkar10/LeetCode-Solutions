e7ff6c94caa1223d7e2d599d71d87b564254d5b3
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2025-10-25 20:17:47 -0500

    Implement crown number pattern functions
    Added new pattern functions to generate various number patterns.

--- Patterns.java
+++ Patterns.java
@@ -1,3 +1,5 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
 class Main {
     static void pattern1(int n){
@@ -114,6 +116,42 @@ class Main {
             System.out.println();
         }
     }
+    static void pattern11(int n){
+        int start;
+        for(int i=0;i<n;i++){
+           if(i%2==0){
+               start = 1;
+           }
+           else start =0;
+           for(int j=0;j<=i;j++){
+               System.out.print(start);
+               start = 1-start;
+           }
+           System.out.println();
+        }
+    }
+    //Number crown pattern
+    static void pattern12(int n){
+        int spaces = 2*(n-1);
+        
+        for(int i = 1;i<=n;i++){
+            //print numbers
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            //print spaces 
+            for(int j=0;j<spaces;j++){
+                System.out.print(" ");
+            }
+            //print numbers in reverse 
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            spaces -=2;
+            System.out.println();
+        }
+    }
+    
     
     
     public static void main(String[] args) {
@@ -130,6 +168,8 @@ class Main {
         //pattern8(n);
         //pattern9(n);
         //pattern10(n);
+        //pattern11(n);
+        //pattern12(n);
         
         
     }
