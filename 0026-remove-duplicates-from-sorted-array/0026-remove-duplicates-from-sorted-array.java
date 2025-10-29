e0b297db20c7e7880f376f308fa8267a7c2b0346
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2025-10-28 20:18:00 -0700

    Time: 0 ms (100%), Space: 44.8 MB (80.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,21 +4,21 @@ class Solution {
         if (nums.length == 0) return 0;
 
         // Pointer for last unique element
-        int i = 0;
+        int j = 0;
 
         // Start from second element
-        for (int j = 1; j < nums.length; j++) {
+        for (int i = 1; i < nums.length; i++) {
             // If new unique element is found
-            if (nums[j] != nums[i]) {
+            if (nums[i] != nums[j]) {
                 // Move unique position forward
-                i++;
+                j++;
                 // Place new unique element
-                nums[i] = nums[j];
+                nums[j] = nums[i];
             }
         }
 
         // i is last index of unique element, count = i + 1
-        return i + 1;
+        return j + 1;
     
 
 
