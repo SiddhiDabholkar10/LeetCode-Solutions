f0164cf76ff1694a6779db79e8b04a31b2e77637
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2025-10-30 22:36:50 -0700

    Time: 0 ms (100%), Space: 46.1 MB (15.22%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int binarySearchSortedArray(int[] nums, int start, int end, int target){
+        
+        if(nums.length == 0 || start>end){
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            return binarySearchSortedArray(nums,mid+1,end,target);
+        }
+        else{
+                return binarySearchSortedArray(nums,start,mid-1,target);
+        }
+            
+        
+        
+    }   
+    public int search(int[] nums, int target) {
+        return binarySearchSortedArray(nums,0,nums.length-1,target);
+    }
+}
