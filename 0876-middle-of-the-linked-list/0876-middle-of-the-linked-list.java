a62b283686b092499b8e3fa3592fc966bdfbacd9
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2026-01-31 22:37:36 -0600

    Time: 0 ms (100%), Space: 43.1 MB (27.42%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode hare = head;
         ListNode tortoise = head;
-        while(hare != null && hare.next!= null && tortoise !=null){
+        while(hare != null && hare.next!= null){
             hare = hare.next.next;
             tortoise = tortoise.next;
 
