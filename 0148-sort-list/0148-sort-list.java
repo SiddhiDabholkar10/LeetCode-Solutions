82dc5d2fc636d39359c4083ef046ef44e316ec4f
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2026-02-09 18:57:31 -0600

    Time: 9 ms (98.58%), Space: 59.5 MB (56.83%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode findMiddle(ListNode head){
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    public ListNode mergeSortedList(ListNode head1, ListNode head2){
+        ListNode t1 = head1;
+        ListNode t2 = head2;
+        ListNode dNode = new ListNode(-1,null);
+        ListNode temp = dNode;
+        while(t1!=null && t2!=null){
+            if(t1.val <= t2.val){
+                temp.next = t1;
+                temp = t1;
+                t1 = t1.next;
+            }
+            else{
+                temp.next = t2;
+                temp = t2;
+                t2 = t2.next;
+            }
+        }
+        if(t1!=null){
+            temp.next = t1;
+        }
+        else{
+            temp.next = t2;
+        }
+        return dNode.next;
+    }
+    public ListNode sortList(ListNode head) {
+        
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode leftHead = head;
+        ListNode middle = findMiddle(head);
+        ListNode rightHead = middle.next;
+        middle.next = null;
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+        head = mergeSortedList(leftHead,rightHead);
+        return head;
+
+    }
+}
