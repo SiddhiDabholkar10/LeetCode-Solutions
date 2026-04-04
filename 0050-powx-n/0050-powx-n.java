3215f193d13d8207e8c86c145b77cd6448907eac
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2026-04-03 21:02:20 -0500

    Time: 0 ms (100%), Space: 48.1 MB (52.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,16 @@
 class Solution {
-    public double myPow(double x, int n) {
-        long N = n;   // use long to safely handle Integer.MIN_VALUE
-        
-        if (N < 0) {
-            x = 1.0 / x;
-            N = -N;
-        }
-        
-        return power(x, N);
+
+    private double power(double x, long n){
+        if(n==0) return 1.0;
+        if(n==1) return x;
+        if(n%2==0) return power((x*x),n/2);
+        return x * power(x, n - 1);
     }
-
-    private double power(double x, long n) {
-        if (n == 0) return 1.0;
-
-        double half = power(x, n / 2);
-
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * x;
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N<0){
+            return 1.0/power(x,-N);
         }
+        return power(x,N);
     }
 }
