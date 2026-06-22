3de75a81f119be8846699fcc3f0f5a0f3c475079
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2026-06-21 21:55:35 -0500

    Time: 1 ms (99.98%), Space: 42.2 MB (97.56%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,12 +1,17 @@
 class Solution {
-    public int reverse(int x) {
-        int rev = 0;
-        while(x != 0){
-            if(rev != rev * 10 / 10)
-                return 0;
-            rev = rev * 10 + x % 10;
-            x /= 10;
+    public int reverse(int num) {
+        long reversed = 0;
+    
+        while (num != 0) {
+            reversed = reversed * 10 + (num % 10);
+            num /= 10;
         }
-        return rev;
+        
+        // Return 0 if the reversed number overflows the standard 32-bit int bounds
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0; 
+        }
+        
+        return (int) reversed;
     }
 }
