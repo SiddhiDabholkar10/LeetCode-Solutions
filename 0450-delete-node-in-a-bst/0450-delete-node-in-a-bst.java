3b81de4e3e1d767a02229e5f285f198301a2493c
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2026-09-19 11:39:31 -0500

    Time: 0 ms (100%), Space: 47.4 MB (56.38%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,9 +15,9 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) return root;
+        if(root == null) return null;
         if(key < root.val) root.left = deleteNode(root.left, key);
-        if(key > root.val) root.right = deleteNode(root.right, key);
+        else if(key > root.val) root.right = deleteNode(root.right, key);
         else{
             if(root.left == null) return root.right;
             if(root.right == null) return root.left;
